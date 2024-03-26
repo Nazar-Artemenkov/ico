f1d43688e9ddb2d2581ee4a992f9da79c9172169
opilane <opilane@HP-CZC2349HTP>
2024-03-26 09:12:20 +0200

    1

--- 26.03/tkinter.py
+++ 26.03/tkinter.py
@@ -1,9 +1,10 @@
 from tkinter import *
+import tkinter
 
-aken=Tk()
+aken= Tk()
 aken.geometry("400x500")
 aken.title("Zagolovok")
 aken.configure("#13e0eb")
 aken.iconbitmap("icon.ico")
-
+pealkiri=Label(aken, bg="#9edb8f",fg="#18420d",cursor="man")
 aken.mainloop()
